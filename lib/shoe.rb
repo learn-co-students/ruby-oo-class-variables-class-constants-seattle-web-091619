99e296ad04f134bb4d7189a63d0add9e1571ff25
github username <github email address>
2019-09-22 15:54:31 -0700

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,26 @@
 class Shoe
+  
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
   def initialize(brand)
     @brand = brand
+    if !BRANDS.include?(brand)
+      BRANDS << brand 
+    end
   end
+
+  BRANDS = []
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
+
+  def brand=(brand)
+    @brand = brand
+    
+  end
+   
 
 end
