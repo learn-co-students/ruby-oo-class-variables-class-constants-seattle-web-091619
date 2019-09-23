43ce19463d0920c6f102ee6f7e2bba379d5cc8cb
Soumya_Goyal <somyagoyal739@gmail.com>
2019-09-23 09:31:42 -0700

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,8 +2,11 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
   def initialize(brand)
     @brand = brand
+    BRANDS.include?(brand)? @brand : BRANDS << @brand
+    
   end
 
   def cobble
@@ -11,4 +14,11 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
+  def brand
+    
+    BRANDS.uniq
+  end
 end
+
+shoe = Shoe.new("nike")
+Soo = Shoe.new("nike")
